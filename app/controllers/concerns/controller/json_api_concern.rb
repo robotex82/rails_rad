ba4c5b4f5185050eda08054cb5643c5b04493922
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2016-02-02 18:07:07 +0100

    Added support for filtering returned attributes via query string: &only=id,title,body Added support for adding included associations via query string: &includes=client,comments

--- app/controllers/concerns/controller/json_api_concern.rb
+++ app/controllers/concerns/controller/json_api_concern.rb
@@ -5,5 +5,38 @@ module Controller
     included do
       respond_to :json
     end
+
+    def index
+      @collection = load_collection
+
+      @collection = add_filter_to_scope(@collection) if (['only'] & params.keys).any?
+      @collection = add_includes_to_scope(@collection) if included_associations.any?
+      @collection = add_count_to_scope(@collection) if params[:count]
+
+      respond_to do |format|
+        format.json {  render json: @collection, include: included_associations  }
+      end
+    end
+
+    private
+
+    def add_filter_to_scope(scope)
+      scope.select(*params[:only].split(',')) if params[:only]
+    end
+
+    def add_includes_to_scope(scope)
+      included_associations.each do |include|
+        scope = scope.send(:includes, include)
+      end
+      scope
+    end
+
+    def add_count_to_scope(scope)
+      scope.count
+    end
+
+    def included_associations
+      params[:includes].respond_to?(:split) ? params[:includes].split(',') : []
+    end
   end
 end
